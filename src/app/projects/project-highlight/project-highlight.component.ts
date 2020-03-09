c2558d6c87fd1d4cdfb524a7f08bb2480a5a0d54
mikejunt <mikejunt@gmail.com>
2020-03-09 11:11:13 -0500

    basic skeleton of welcome

--- src/app/projects/project-highlight/project-highlight.component.ts
+++ src/app/projects/project-highlight/project-highlight.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-project-highlight',
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-highlight.component.scss']
 })
 export class ProjectHighlightComponent implements OnInit {
+@Input() highlightid: string
 
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {  }
 
 }
