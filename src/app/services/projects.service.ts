6b5414331d6d59b3c1b0613e4135adbfd6fb63f9
mikejunt <mikejunt@gmail.com>
2020-03-09 22:18:06 -0500

    welcome reading state & filtering to children

--- src/app/services/projects.service.ts
+++ src/app/services/projects.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
-import { Project } from '../interfaces/project.interface';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-// import * as Actions from '../store/actions';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store';
+import * as Actions from '../store/actions';
 // import * as Selectors from '../store/selectors';
 
 @Injectable({
@@ -10,14 +10,14 @@ import { Observable } from 'rxjs';
 })
 export class ProjectsService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private store: Store<AppState>) { }
 
   getProjects() {
-    this.http.get('/api/projects/all').subscribe(res=>console.log(res, "all projects"))
+    this.http.get('/api/projects/all').subscribe(res=>this.store.dispatch(Actions.saveProjects({projects: res['data']})))
   }
 
   getFeatures() {
-    this.http.get('/api/projects/feature').subscribe(res=>console.log(res,"features"))
+    this.http.get('/api/projects/feature').subscribe(res=>this.store.dispatch(Actions.saveFeatures({features: res['data']})))
   }
 
   saveProject(project: Object) {
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { AppState } from '../store';
+import * as Selectors from '../store/selectors';
+import { Project } from '../interfaces/project.interface';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-welcome',
@@ -6,8 +11,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.scss']
 })
 export class WelcomeComponent implements OnInit {
+  features$: Observable<Project[]>;
+  featureids: Array<string> = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) {this.features$ = this.store.select(Selectors.viewFeatures)
+  this.features$.subscribe(res=> 
+    {if (res.length != 0) {res.forEach(obj => {this.featureids.push(obj["_id"])})}}) }
 
   ngOnInit(): void { 
   }
