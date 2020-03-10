7fe62f70524cc006cd2a481d4da2cddde2902471
mikejunt <mikejunt@gmail.com>
2020-03-10 10:27:55 -0500

    highlight communicates

--- src/app/projects/project-detail/project-detail.component.ts
+++ src/app/projects/project-detail/project-detail.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
 import * as Selectors from '../../store/selectors'
 import { Observable } from 'rxjs';
+import { Project } from 'src/app/interfaces/project.interface';
 
 @Component({
   selector: 'app-project-detail',
@@ -12,9 +13,20 @@ import { Observable } from 'rxjs';
 export class ProjectDetailComponent implements OnInit {
   curproj$: Observable<string>
   curproj: string
+  projects$: Observable<Project[]>
+  project: Project
 
   constructor(private store: Store<AppState>) {this.curproj$ = this.store.select(Selectors.viewCurrentProject)
-  this.curproj$.subscribe(res=>this.curproj = res) }
+  this.curproj$.subscribe(res=>this.curproj = res)
+  this.projects$ = this.store.select(Selectors.viewAllProjects)
+  this.projects$.subscribe(res=> {
+    if (res != []) {
+      console.log(res, "array from detail comp")
+    let filteredproj = res.filter(obj=> obj._id === this.curproj)
+    console.log(filteredproj, "post-filter from detail comp")
+    this.project = filteredproj[0]
+    }})
+   }
 
   ngOnInit(): void {
   }
--- src/app/projects/project-highlight/project-highlight.component.ts
+++ src/app/projects/project-highlight/project-highlight.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store';
-import * as Selectors from '../../store/selectors'
+import * as Selectors from '../../store/selectors';
+import * as Actions from '../../store/actions';
 import { Observable } from 'rxjs';
 import { Project } from 'src/app/interfaces/project.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-highlight',
@@ -15,10 +17,15 @@ export class ProjectHighlightComponent implements OnInit {
 features$: Observable<Project[]>
 feature: Project
 
-  constructor(private store: Store<AppState>) {this.features$ = this.store.select(Selectors.viewFeatures);
+  constructor(private store: Store<AppState>, private router: Router) {this.features$ = this.store.select(Selectors.viewFeatures);
    }
 
   ngOnInit(): void {  this.features$.subscribe(res=> {if (res) {this.feature = res[this.highlightid]}});
   console.log(this.feature,"feature", this.highlightid, "highlightid")}
+
+  projectnav(id: string) {
+    this.store.dispatch(Actions.setViewedProject({project: id}))
+    this.router.navigate(['/projects'])
+  }
 
 }
