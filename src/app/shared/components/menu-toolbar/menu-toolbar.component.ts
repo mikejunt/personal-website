4752244553859b6d38eaf88e2542116aff74e55f
mikejunt <mikejunt@gmail.com>
2020-03-11 21:36:46 -0500

    responsive toolbar

--- src/app/shared/components/menu-toolbar/menu-toolbar.component.ts
+++ src/app/shared/components/menu-toolbar/menu-toolbar.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-menu-toolbar',
   templateUrl: './menu-toolbar.component.html',
   styleUrls: ['./menu-toolbar.component.scss']
 })
-export class MenuToolbarComponent implements OnInit {
+export class MenuToolbarComponent implements OnInit, OnDestroy {
+  mobileQuery: MediaQueryList
+  private mobileQueryListener: () => void
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 420px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+   }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void { this.mobileQuery.removeListener(this.mobileQueryListener) }
 
   navigate(path: string) {
     this.router.navigate([path])
