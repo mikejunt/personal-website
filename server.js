45f4505d217315282f8f6e5790ff2c03c4aa752e
mikejunt <mikejunt@gmail.com>
2020-03-12 16:33:55 -0500

    deployment prep

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const mongooseConf = require('./server/config/mongo.conf')
