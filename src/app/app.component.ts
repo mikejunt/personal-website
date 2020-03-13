c975b6a4831333af1b92879a5deac1380ffbb5a8
mikejunt <mikejunt@gmail.com>
2020-03-13 10:41:32 -0500

    fixes scroll issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { ProjectsService } from './services/projects.service';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -8,5 +9,8 @@ import { ProjectsService } from './services/projects.service';
 })
 export class AppComponent {
   title = 'personal-website';
-  constructor (private projects: ProjectsService) {this.projects.getFeatures();this.projects.getProjects()}
+  constructor (private projects: ProjectsService, private router: Router) {this.projects.getFeatures();this.projects.getProjects(),
+  this.router.events.subscribe(event=> {
+    if (event instanceof NavigationEnd) {
+      window.scrollTo(0,0)}})}
 }
